3b76e29ec43c012340c9bbfa732565df600cf17e
MD Pahlovi <mdpahlovi@outlook.com>
2025-10-04 22:24:23 +0600

    refactor(config): remove unused proxy and url configs

--- backend/config/admin.ts
+++ backend/config/admin.ts
@@ -17,11 +17,4 @@ export default ({ env }) => ({
         nps: env.bool("FLAG_NPS", true),
         promoteEE: env.bool("FLAG_PROMOTE_EE", true),
     },
-    cookies: {
-        secure: env("NODE_ENV") === "production",
-        httpOnly: true,
-        sameSite: "lax",
-    },
-    proxy: true,
-    url: env("PUBLIC_URL"),
 });
